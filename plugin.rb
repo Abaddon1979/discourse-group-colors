6a334411b95f0d08aa9063756482325e7ced7b26
Abaddon1979 <77430838+Abaddon1979@users.noreply.github.com>
2024-11-17 20:33:44 -0500

    a

--- plugin.rb
+++ plugin.rb
@@ -9,11 +9,13 @@ enabled_site_setting :group_colors_enabled
 register_asset 'stylesheets/group-colors.scss'
 register_svg_icon "fa-shield-halved"
 
+add_admin_route 'group_colors.title', 'group-colors'
+
 after_initialize do
   # Register custom fields for groups
   Group.register_custom_field_type('color', :string)
   Group.register_custom_field_type('color_rank', :integer)
-  
+
   add_to_serializer(:basic_group, :color) { object.custom_fields['color'] }
   add_to_serializer(:basic_group, :color_rank) { object.custom_fields['color_rank'].to_i if object.custom_fields['color_rank'].present? }
   
@@ -25,6 +27,7 @@ after_initialize do
     return nil if groups.empty?
     
     if SiteSetting.group_colors_priority_enabled
+      # Sort by rank (lower number = higher priority)
       highest_priority_group = groups.min_by { |g| g.custom_fields['color_rank'].to_i || 999 }
       highest_priority_group.custom_fields['color']
     else
@@ -32,11 +35,14 @@ after_initialize do
     end
   end
   
+  # Add group color management endpoints
   require_dependency 'groups_controller'
   class ::GroupsController < ::ApplicationController
     before_action :ensure_logged_in, only: [:update_color]
     
     def update_color
+      return render json: failed_json unless SiteSetting.group_colors_enabled
+      
       group = Group.find(params[:id])
       guardian.ensure_can_edit!(group)
 
@@ -53,7 +59,31 @@ after_initialize do
     end
   end
 
+  # Add routes
   Discourse::Application.routes.append do
     put '/groups/:id/color' => 'groups#update_color'
+    get '/admin/plugins/group-colors' => 'admin/plugins#index', constraints: StaffConstraint.new
+  end
+
+  # Initialize colors for existing groups if needed
+  DiscourseEvent.on(:site_setting_saved) do |name, old_value, new_value|
+    if name == :group_colors_enabled && new_value == true
+      Group.where(automatic: false).find_each do |group|
+        if group.custom_fields['color'].blank?
+          group.custom_fields['color'] = "##{SecureRandom.hex(3)}"
+          group.custom_fields['color_rank'] = 999 # Default to lowest priority
+          group.save_custom_fields(true)
+        end
+      end
+    end
+  end
+
+  # Handle new group creation
+  DiscourseEvent.on(:group_created) do |group|
+    if SiteSetting.group_colors_enabled && group.custom_fields['color'].blank?
+      group.custom_fields['color'] = "##{SecureRandom.hex(3)}"
+      group.custom_fields['color_rank'] = 999 # Default to lowest priority
+      group.save_custom_fields(true)
+    end
   end
 end
